7f42dd9509caff5103f9ab6758a54ac8a298b62b
Sam. K <sam.ds@live.com>
2022-05-01 13:27:40 +0200

    testing api requests

--- testing/js/script.js
+++ testing/js/script.js
@@ -19,3 +19,15 @@ $('document').ready(function (){
         $("#navigation").toggle()
     })
 });
+
+getW();
+
+async function getW(){
+    let apikey = "860343fcf564f99b3fb00f9536403ad7";
+    //let query = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=";
+    let query = "https://api.openweathermap.org/data/2.5/weather?q=Milan,ITA&units=metric&appid=";
+    var reply = await
+        fetch(query+apikey,{method:"GET"});
+    var jsonObj = await reply.json();
+    console.log(jsonObj);
+}
