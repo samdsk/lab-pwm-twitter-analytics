7a5c7b6c7b8a40fd598ad42f81186b1ac438fac7
Sam. K <sam.ds@live.com>
2022-05-06 11:03:15 +0200

    temp testing

--- testing/js/script.js
+++ testing/js/script.js
@@ -20,14 +20,40 @@ $('document').ready(function (){
     })
 });
 
-getW();
 
-async function getW(){
+let city_tags = document.getElementById("weather-carousel").getElementsByClassName("city-name")
+let cities = [];
+Array.from(city_tags).forEach(e => {
+    cities.push(e.innerHTML)
+});
+
+console.log(cities);
+
+getW(cities);
+
+function getW(cities){
     let apikey = "860343fcf564f99b3fb00f9536403ad7";
     //let query = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=";
-    let query = "https://api.openweathermap.org/data/2.5/weather?q=Milan,ITA&units=metric&appid=";
-    var reply = await
-        fetch(query+apikey,{method:"GET"});
-    var jsonObj = await reply.json();
-    console.log(jsonObj);
+    cities.forEach(async function(city){
+        let query = "https://api.openweathermap.org/data/2.5/weather?q="+city+",ITA&units=metric&appid=";
+        var reply = await
+         fetch(query+apikey,{method:"GET"});
+        var jsonObj = await reply.json();
+        console.log(jsonObj.main.temp)
+        
+        var temp = document.getElementById("weather-carousel").getElementsByClassName("weather-data")
+        console.log(temp)
+        for (let iterator of temp) {
+            
+            if(iterator.getElementsByTagName("h3")[0].innerText==city){
+                var temp_node = document.createElement("li").appendChild(document.createTextNode(jsonObj.main.temp))
+                
+                iterator.getElementsByTagName("ul")[0].append(temp_node)
+            }
+                
+        }
+
+        
+    });
+    
 }
