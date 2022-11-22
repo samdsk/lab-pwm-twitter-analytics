d1de10d15d36367262e38585b45e557f552de836
Sam. K <sam.ds@live.com>
2022-11-22 11:35:54 +0100

    main pages

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+
+
 app.use(express.static(__dirname + '/public'))
+
 app.set('view engine','ejs')
 
 app.get('/',function(req,res){
     res.render('pages/index')
 
 })
+
+app.get('/signup',function(req,res){
+    res.render('pages/singup')
+})
+
+app.get('/about',function(req,res){
+    res.render('pages/about')
+})
+
 
 app.listen(3000)
 
