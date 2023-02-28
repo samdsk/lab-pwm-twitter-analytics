a4dbb754957595b36a603c893980e2974f253ce6
Sam. K <sam.ds@live.com>
2023-02-28 13:31:40 +0100

    printgin search results on web page

--- controllers/twitter.js
+++ controllers/twitter.js
@@ -6,13 +6,13 @@ const mongoose = require('mongoose')
 const SearchResults = require('../models/SearchResults')
 const User = require('../models/User')
 
-// ! TESTING MODE - OFF
-// ! WRITING TO DATA.JSON
-// // ! file is in TESTING MODE
-// // ! THERE ARE NO API REQUESTS SEND
-// // ! READING FROM data.json
+// // ! TESTING MODE - OFF
+// // ! WRITING TO DATA.JSON
+// ! file is in TESTING MODE
+// ! THERE ARE NO API REQUESTS SEND
+// ! READING FROM data.json
 const testing_filename = './data.json'
-const filename = './data.json'
+const filename = './data_asd.json'
 
 const tweet_fields = [
     'attachments', 
@@ -35,28 +35,45 @@ const tweet_media_fields = [
     'media_key','type','url',
 ]
 
+const tweet_user_fields = [
+    "created_at", 
+    "description",
+    "entities", 
+    "id", 
+    "location", 
+    "name", 
+    "pinned_tweet_id", 
+    "profile_image_url", 
+    "protected", 
+    "public_metrics", 
+    "url", 
+    "username", 
+    "verified", 
+    "verified_type", 
+    "withheld"
+]
+
 // // ! testing mode
 const search = async (ID) => { 
-    res = await app.search(`from:${ID}`,{
-        'tweet.fields':tweet_fields,
-        'expansions':['attachments.media_keys'],
-        'media.fields':tweet_media_fields})
+    // res = await app.search(`from:${ID}`,{
+    //     'tweet.fields':tweet_fields,
+    //     'expansions':['attachments.media_keys'],
+    //     'media.fields':tweet_media_fields})
     
-    let data = await res.fetchLast()    
+    // let data = await res.fetchLast()    
     // // ! testing mode : set data variable
-    return new Promise( (resolve,reject) => fs.writeFile(filename,JSON.stringify(data), (err)=>{
+    return new Promise( (resolve,reject) => fs.writeFile(filename,JSON.stringify("testing"), (err)=>{
         if(err) reject(err)
         console.log(">>> File created successfully !")
         
-        resolve(data_process(filename))
+        resolve(data_process(testing_filename))
     })) 
 }
 
 const postTwitter = async(req,res,next) => {
     console.log("Handler: "+req.body.handler)
-    const user = await app.userByUsername(req.body.handler,{"user.fields":"public_metrics"});
-    if(user?.errors) return res.json(JSON.stringify(`Error: Invalid user`))
-
+    const user = await app.userByUsername(req.body.handler,{"user.fields":tweet_user_fields});
+    if(user?.errors) return res.json(JSON.stringify(`Error: Invalid user`))    
 
     // console.log("request for ->",user.data);
     // let liked_tweets = await (await app.userLikedTweets(id)).fetchLast(100)
@@ -73,20 +90,22 @@ const postTwitter = async(req,res,next) => {
         data.followers = user.data.public_metrics.followers_count
         data.followings = user.data.public_metrics.following_count
         data.total_tweets = user.data.public_metrics.tweet_count
-        //console.log(data);
+        data.user_img = user.data.profile_image_url
+        data.date = new Date()
+        //console.log(data);        
 
         // // ! testing mode
-        console.log('creating search results')
-        let count = await SearchResults.find({username:req.body.handler})
-        console.log(count.length);
+        // console.log('creating search results')
+        // let count = await SearchResults.find({username:req.body.handler})
+        
 
-        if(count.length > 1){
-            await SearchResults.findOneAndRemove({_id:count[0]._id})
-            await User.updateOne({username:req.session.username},{$pull : {searched:count[0]._id}})
-        }
+        // if(count.length > 1){
+        //     await SearchResults.findOneAndRemove({_id:count[0]._id})
+        //     await User.updateOne({username:req.session.username},{$pull : {searched:count[0]._id}})
+        // }
 
-        await SearchResults.create(data)
-        await User.findOneAndUpdate({name:req.session.username},{$push : {searched:data._id}}).orFail(new Error("User update failed."))
+        // await SearchResults.create(data)
+        // await User.findOneAndUpdate({name:req.session.username},{$push : {searched:data._id}}).orFail(new Error("User update failed."))
 
         delete(data._id)
         res.json(JSON.stringify(data))
