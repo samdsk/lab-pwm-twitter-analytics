d39344b68aedb7280b05f41e90184bb352671575
Sam. K <sam.ds@live.com>
2023-03-21 17:17:11 +0100

    rendering searched results

--- controllers/history.js
+++ controllers/history.js
@@ -8,13 +8,12 @@ const getSearchedResults = async (email) => {
     let searched = await User.findOne({_id:user._id},'searched')
     searched = searched.searched
     const search_ids = []
-    const projections = `_id date name user_img username 
+    const projections = `_id date name user_img username
     start_date end_date followings followers total_tweets total.count`;
 
     await Promise.all(
         searched.map( async (id) => {
-            let temp = await SearchResults.findById(id,projections)           
-            console.log(temp.total.count);
+            let temp = await SearchResults.findById(id,projections)
             search_ids.push(temp)
         })
     )
@@ -23,10 +22,17 @@ const getSearchedResults = async (email) => {
 }
 
 const getHistory = async (req,res,next) => {
+    if(req.query.error) return res.render('pages/history',{
+        logout:true,
+        username:req.session.username,
+    })
+
     let results = await getSearchedResults(req.session.email)
+
     results = results.sort( (a,b)=>{
         return new Date(a.date) < new Date(b.date) ? 1 : -1
     })
+
     res.render('pages/history',{
         logout:true,
         username:req.session.username,
