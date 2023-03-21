0fe05da8bdce8c0866fe3cc6de41fc436360bba0
Sam. K <sam.ds@live.com>
2023-03-21 17:25:01 +0100

    results page init

---
+++ controllers/results.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Auth = require('../models/Auth')
+const User = require('../models/User')
+const SearchResults = require('../models/SearchResults')
+
+
+
+const getResults = async (req,res,next) => {
+
+}
+
+module.exports = {getResults}
