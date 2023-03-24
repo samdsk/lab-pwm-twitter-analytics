db5280bb05b985e150041551f65100d9e389553b
Sam. K <sam.ds@live.com>
2023-03-24 14:14:03 +0100

    added remove result record, removed comments, added error msg modal

--- controllers/results.js
+++ controllers/results.js
@@ -3,10 +3,11 @@ const Auth = require('../models/Auth')
 const User = require('../models/User')
 const SearchResults = require('../models/SearchResults')
 
-async function validateID(id){
+async function validateID(id,email){
+
     if(!mongoose.isValidObjectId(id)) return false
 
-    let user_id = await Auth.findOne({email:req.session.email})
+    let user_id = await Auth.findOne({email:email})
     let searched = await User.findById(user_id._id,'searched')
 
     if(!searched.searched.includes(id)){
@@ -18,12 +19,14 @@ async function validateID(id){
 
 
 const getResults = async (req,res,next) => {
-
+    let email = req.session.email
     if(req.query.compare > 0){
 
         if(!req.query.id.length) return res.sendStatus(400)
         if(req.query.id.length != 2) return res.sendStatus(400)
-        if(!(validateID(req.query.id[0]) && validateID(req.query.id[1])))
+
+        if(!(await validateID(req.query.id[0],email)
+            && await validateID(req.query.id[1],email)))
             return res.sendStatus(400)
 
         let result_1 = await SearchResults.findById(req.query.id[0])
@@ -32,7 +35,7 @@ const getResults = async (req,res,next) => {
         res.json(JSON.stringify([result_1,result_2]))
 
     }else{
-        if(!(validateID(req.query.id)))
+        if(!(await validateID(req.query.id,email)))
             return res.sendStatus(400)
 
         let result = await SearchResults.findById(req.query.id)
@@ -42,8 +45,19 @@ const getResults = async (req,res,next) => {
 
 }
 
+// NOTE remove disabled
 const removeResult = async (req,res,next) => {
-    res.sendStatus(200)
+
+    if(!req.body.id) return res.sendStatus(400)
+    if(! await validateID(req.body.id,req.session.email)) return res.sendStatus(400)
+
+    // SearchResults.findByIdRemove(req.body.id).exec(function(err,item){
+    //     if(err) return res.sendStatus(400)
+    //     if(!item) return res.sendStatus(404)
+
+    // })
+
+    return res.sendStatus(200)
 }
 
 module.exports = {getResults,removeResult}
