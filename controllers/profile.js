ffd004216dadbbe5a5a7d21380254e3e35f12055
Sam. K <sam.ds@live.com>
2023-03-27 18:58:33 +0200

    profile change and delete

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,13 +1,65 @@
+const bcrypt = require('bcrypt')
+const Auth = require("../models/Auth")
+const SearchResults = require('../models/SearchResults')
 const getProfile = async (req,res,next) => {
-    req.session.email
-    res.render('pages/profile',
-        {
-            logout:true,
-            username:req.session.username,
-            email: req.session.email
+
+    res.render('pages/profile',{
+        logout:true,
+        username:req.session.username,
+        email: req.session.email,
+        email_hash:req.session.gravatar
+    })
+}
+
+const postProfile = async (req,res,next) => {
+    if(req.session.email){
+        if(req.body.change_psw){
+            const password = await bcrypt.hash(req.body.signup_password,10)
+            Auth.findOneAndUpdate({email:req.session.email},{password:password}).exec((err,data)=>{
+                if(err) {
+                    res.sendStatus(500)
+                    throw new Error("Profile change: Auth error")
+                }
+            })
+            res.sendStatus(200)
+        }
+    }
+}
+const deleteProfile = async (req,res,next) => {
+    if(req.session.email){
+
+        let id = await Auth.findOne({email:req.session.email},'_id')
+        let searched = await User.findById(_id,'searched')
+        await Promise.all(
+            searched.searched.map(async(x)=>{
+                SearchResults.findByIdAndRemove(x).exec((err,data)=>{
+                    if(err) {
+                        res.sendStatus(500)
+                        throw new Error("Profile delete: SearchResults error")
+                    }
+                })
+            })
+        )
+
+        Auth.findByIdAndRemove(id._id).exec((err,data)=>{
+            if(err) {
+                res.sendStatus(500)
+                throw new Error("Profile delete: Auth error")
+            }
         })
+
+        User.findByIdAndRemove(id._id).exec((err,data)=>{
+            if(err) {
+                res.sendStatus(500)
+                throw new Error("Profile delete: User error")
+            }
+        })
+
+        res.sendStatus(200)
+
+    }
 }
 
 
 
-module.exports = getProfile
+module.exports = {getProfile,postProfile,deleteProfile}
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const dashboard = require('../controllers/dashboard')
-const profile = require('../controllers/profile')
+const {getProfile,postProfile,deleteProfile} = require('../controllers/profile')
 const history = require('../controllers/history')
 const search = require('../controllers/search')
 
-router.get('/profile',profile)
+router.get('/profile',getProfile).post(postProfile).delete(deleteProfile)
 router.get('/history',history)
 router.get('/search',search)
 router.get('/',dashboard)
