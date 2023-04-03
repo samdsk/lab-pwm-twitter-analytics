36770b47f0960c2fb7914720c703890ae0c28fe4
Sam. K <sam.ds@live.com>
2023-04-03 17:54:18 +0200

    captcha file

---
+++ utils/recaptcha.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch')
+
+
+const recaptcha = async(captcha)=>{
+    if(!captcha || captcha == '') return false
+
+    const SECRET_KEY = process.env.CAPTCHA_SECRET;
+    const VERIFY_URL = `https://google.com/recaptcha/api/siteverify`;
+
+    // Make a request to verifyURL
+    const body = await fetch(VERIFY_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: `secret=${SECRET_KEY}&response=${captcha}`
+    }).then(res => res.json());
+
+    if(!body.success) return false
+    return true
+}
+
+module.exports = recaptcha
