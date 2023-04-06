28152b52e17f198cba5f9580afd4c3e286409301
Sam. K <sam.ds@live.com>
2023-04-06 20:48:01 +0200

    minor fixes

--- controllers/login.js
+++ controllers/login.js
@@ -6,22 +6,22 @@ const SessionDuration = 1000 * 60 * 60 * 60
 const md5 = require('md5')
 
 const login = async (req,res,next) => {
-
     const {email, password, remember} = req.body
+    console.log("Login: new request received for:",email);
 
     Auth.findOne({email:email}, async (err,auth)=> {
         if(auth == null) return res.redirect("/?error=Invalid credentials")
 
         await bcrypt.compare(password,auth.password).then(async (check)=>{
             if(!check) return res.redirect("/?error=Invalid credentials")
-            console.log("here");
+
             const username = await User.findOne({_id:auth._id}).populate('_id')
             const email_hash = md5(email.trim().toLowerCase())
 
             req.session.username = username.name
             req.session.email = username._id.email
             req.session.gravatar = email_hash
-            console.log(req.session);
+
             // ! session maxage
             if(remember === "on") req.session.cookie.maxAge = SessionDuration
 
